5b83489febb45c8fd49ede4f5dc103677d73870d
Cole Alves <cole101013@yahoo.com>
2015-10-10 20:46:32 -0500

    Birds are flying everywhere

---
+++ ScatterJam2015/Assets/Scripts/BirdSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdSpawn : MonoBehaviour {
+
+    public GameObject _bird = null;
+    public int _birdCount = 50;
+    public float _birdRadius = 300.0f;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < _birdCount; i++)
+        {
+            Vector3 v = Random.insideUnitSphere * _birdRadius;
+            Instantiate(_bird, v, Quaternion.identity);
+        }
+	}
+}
