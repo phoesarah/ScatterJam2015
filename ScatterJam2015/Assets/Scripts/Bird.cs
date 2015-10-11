8d439e593c61a8b4dd2e61c336e5b9ac0ba29cf5
Cole Alves <cole101013@yahoo.com>
2015-10-10 19:33:21 -0500

    Bird
    The bird is flying fine.

---
+++ ScatterJam2015/Assets/Scripts/Bird.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Bird : MonoBehaviour {
+
+    
+    // Use this for initialization
+
+    void Start () {
+       
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.position += transform.forward * -0.05f;
+        transform.Rotate(transform.up, 1.0f); //Random.Range(-10F, 10F));
+    }
+}
