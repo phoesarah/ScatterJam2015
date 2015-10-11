a5f4a6595df9806b4b03338faf74136ec2173cdc
gfetters <gfetters@gmail.com>
2015-10-11 07:34:02 -0500

    Game Scene 2 work

---
+++ ScatterJam2015/Assets/Scripts/Arrow2Display2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow2Display2 : MonoBehaviour {
+	int flipper;
+	float x;
+	
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("Arrow2").active = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		}
+		
+}
